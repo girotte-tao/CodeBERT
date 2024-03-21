769e182146902781fc26208a6ea427528b971d61
girotte-tao <taoware@qq.com>
2024-03-21 10:27:37 +0800

    train bash

--- GraphCodeBERT/clonedetection/train.sh
+++ GraphCodeBERT/clonedetection/train.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
 mkdir -p saved_models
+
+# 定义日志目录和文件名
+LOG_DIR="log"
+LOG_FILE="${LOG_DIR}/$(date +%Y-%m-%d_%H-%M-%S).log"
+
+# 确保日志目录存在
+mkdir -p ${LOG_DIR}
+
+# 运行脚本并将输出重定向到日志文件
 python run.py \
     --output_dir=saved_models \
     --config_name=microsoft/graphcodebert-base \
@@ -11,9 +21,9 @@ python run.py \
     --epoch 1 \
     --code_length 512 \
     --data_flow_length 128 \
-    --train_batch_size 16 \
-    --eval_batch_size 32 \
+    --train_batch_size 4 \
+    --eval_batch_size 4 \
     --learning_rate 2e-5 \
     --max_grad_norm 1.0 \
     --evaluate_during_training \
-    --seed 123456 2>&1| tee saved_models/train.log
+    --seed 123456 > "${LOG_FILE}" 2>&1
